579b64e8ed05c64305acd3459c148c927037aabb
heitordeve <heitor.developer@outlook.com>
2020-04-12 21:01:10 -0300

    Session 5 - Class 2

---
+++ modules/module-01.js
@@ -0,0 +1,11 @@
+console.log('Excutando module-01')
+
+module.exports.oculta = ()=>{
+    console.log('Executando a função oculta')
+}
+
+module.exports.executa = ()=>{
+    console.log('executando a função executa')
+}
+
+module.exports.welcome = `Bem vindo ao módulo module-01 `
