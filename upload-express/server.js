c1771a86304c26faeed8545bfbd09484ecccb324
heitordeve <heitor.developer@outlook.com>
2020-04-16 22:12:17 -0300

    Session 9 - Class 3

--- upload-express/server.js
+++ upload-express/server.js
@@ -3,13 +3,26 @@ const bodyParser = require('body-parser')
 const multer = require('multer')
 
 const app = express()
+const path = require('path')
 
 app.use(bodyParser.urlencoded({extended:true}))
 
 app.get('/',(req, res)=>{
     //res.json({message: 'Bem Vindo'})
-    res.sendfile(__dirname+'/index.html') 
+    res.sendFile(__dirname+'/index.html') 
 }) 
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb)=>{
+        cb(null, 'uploads/')
+    },
+    filename: (req, file, cb)=>{
+        cb(null, file.fieldname+'-'+Date.now()+path.extname(file.originalname))
+    }
+})
+
+const upload = multer({storage})
+
 
 app.listen(3000, '127.0.0.1',()=>{
     console.log(`Server Running on port 3000`)
