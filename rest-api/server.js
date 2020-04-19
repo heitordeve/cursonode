436cb189f55a7261f910aad2dbf5ebc6d597c6fc
heitordeve <heitor.developer@outlook.com>
2020-04-18 22:03:04 -0300

    Session 11 - Class 4

--- rest-api/server.js
+++ rest-api/server.js
@@ -2,6 +2,13 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 
+mongoose.connect("mongodb+srv://user_hcode:Hcs@83451836*@cluster0-cbtqe.mongodb.net/subscribers?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true })
+
+const db = mongoose.connection
+
+db.on('err', (err)=> console.log('err'))
+db.once('open', ()=> console.log('Mongo = OK'))
+
 app.listen(3000, ()=>{
     console.log("Servidor http = OK")
 })
