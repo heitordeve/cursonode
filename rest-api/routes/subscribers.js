cc31ae35957c799e733a8f8a6ba0cf3a712e775a
heitordeve <heitor.developer@outlook.com>
2020-04-18 22:36:54 -0300

    Session 11 - Class 6

---
+++ rest-api/routes/subscribers.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const router = express.Router()
+
+router.get('/', (res, req)=>{
+    
+})
+
+router.get('/:id', (res, req)=>{
+
+})
+
+router.post('/', (res, req)=>{
+
+})
+
+router.patch('/:id', (res, req)=>{
+
+})
+
+router.delete('/:id', (res, req)=>{
+
+})
+
+
+
+module.exports = router
