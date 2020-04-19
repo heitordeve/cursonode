c8fbef5434c9d243a3904075fcf3915607a229e8
heitordeve <heitor.developer@outlook.com>
2020-04-18 22:09:39 -0300

    Session 11 - Class 5

--- rest-api/server.js
+++ rest-api/server.js
@@ -1,13 +1,16 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 
-mongoose.connect("mongodb+srv://user_hcode:Hcs@83451836*@cluster0-cbtqe.mongodb.net/subscribers?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.DATABASE_STRING, { useNewUrlParser: true, useUnifiedTopology: true })
 
 const db = mongoose.connection
 
 db.on('err', (err)=> console.log('err'))
 db.once('open', ()=> console.log('Mongo = OK'))
+
+app.use(express.json())
 
 app.listen(3000, ()=>{
     console.log("Servidor http = OK")
