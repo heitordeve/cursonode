7b7e568b13e8526c474d6c5b35c9b05df715c830
heitordeve <heitor.developer@outlook.com>
2020-04-19 21:33:29 -0300

    Session 11 - Class 11

--- rest-api/routes/subscribers.js
+++ rest-api/routes/subscribers.js
@@ -12,7 +12,7 @@ router.get('/', async (req, res)=>{
     }
 }) 
 
-router.get('/:id', (req, res)=>{
+router.get('/:id', getSubscriber, (req, res)=>{
 
 })
 
@@ -30,14 +30,26 @@ router.post('/', async (req, res)=>{
     }
 })
 
-router.patch('/:id', (req, res)=>{
+router.patch('/:id', getSubscriber , (req, res)=>{
 
 })
 
-router.delete('/:id', (req, res)=>{
+router.delete('/:id', getSubscriber, (req, res)=>{
 
 })
 
+async function getSubscriber(req, res, next){
+    try {
+        subscriber = await Subscriber.findById(req.params.id)
+        if(subscriber == null){
+            return res.status(404).json({message:'subscriber not found'})
+        }
+    } catch (error) {
+        return res.status(500).json({message: error})
+    }
 
+    res.subscriber = subscriber
+    next()
+}
 
 module.exports = router
